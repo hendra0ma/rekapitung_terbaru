5fa0695819b33543f4be0c7727da473de3346646
hendra0ma <hendra0maulidan@gmail.com>
2022-12-21 11:09:55 +0700

    berakhir tutup di awan

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -2,6 +2,7 @@
 
 use App\Models\Config;
 use App\Models\District;
+use App\Models\Province;
 use App\Models\Regency;
 use App\Models\Tps;
 use Illuminate\Support\Facades\DB;
@@ -37,7 +38,7 @@ $tps = 2963;
                 </a><!-- LOGO -->
             </div>
             <ul class="side-menu">
-                <li class="my-2">
+                <!-- <li class="my-2">
                     &nbsp;
                 </li>
                 <li class="mt-5">
@@ -53,10 +54,36 @@ $tps = 2963;
                             </center>
                         </a>
                     </span>
+                </li> -->
+
+                <li class="my-2">
+                    &nbsp;
+                </li>
+                <li class="mt-5">
+                    <center>
+                        <img src="{{asset('images/logo')}}/rekapitung_gold.png" style="width:120px;height:auto">
+                    </center>
                 </li>
                 <li>
                     <h3>Main Count</h3>
                 </li>
+
+                <li class="slide">
+                <?php
+                    $props = Province::where('id',$kota['province_id'])->first();
+                    $cityProp = Regency::where('province_id',$kota['province_id'])->get();
+                
+                ?>
+                    <a class="side-menu__item" data-bs-toggle="slide" href="#"><i
+                            class="side-menu__icon fe fe-grid"></i><span class="side-menu__label">PROVINSI {{$props->name}}</span><i
+                            class="angle fa fa-angle-right"></i></a>
+                    <ul class="slide-menu">
+                        @foreach($cityProp as $cp)
+                        <li><a href="{{url('')}}/administrator/absensi" class="slide-item">{{$cp->name}}</a></li>
+                        @endforeach
+                    </ul>
+                </li>
+
                 <!-- <li>
                     <a class="side-menu__item" href="{{url('')}}/administrator/absensi"><i class="side-menu__icon mdi mdi-printer"></i><span class="side-menu__label">Absensi Saksi</span></a>
                 </li> -->
